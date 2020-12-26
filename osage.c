2a92b9c3664f1fc1140d59b29588372652a1f9c1
Syed Ali <syedali.razashah1234@gmail.com>
2020-12-26 05:07:44 +0500

    Update osage.c

--- osage.c
+++ osage.c
@@ -1,4 +1,4 @@
-mov ah, 0x0e ; tty mode
+mov ah, 0x0e
 mov al, 'B'
 int 0x10
 mov al, 'o'
@@ -14,10 +14,7 @@ int 0x10
 mov al, 'u'
 int 0x10
 mov al, 'x'
-int 0x10   
-
-jmp $ ; jump to current address = infinite loop
-
-; padding and magic number
+int 0x10
+jmp $
 times 510 - ($-$$) db 0
 dw 0xaa55 
