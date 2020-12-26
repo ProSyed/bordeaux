d60ea810fe0a68a3354c70661720b95457844fca
Syed Ali <syedali.razashah1234@gmail.com>
2020-12-26 05:06:05 +0500

    Update osage.c

--- osage.c
+++ osage.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
+mov ah, 0x0e ; tty mode
+mov al, 'B'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'd
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 'u'
+int 0x10
+mov al, 'x'
+int 0x10   
+
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55 
